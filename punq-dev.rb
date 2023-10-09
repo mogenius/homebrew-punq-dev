5abcba15541e1f471ee005251767823bdc8845c3
punq <punqs@mogenius.com>
2023-10-09 20:23:33 +0000

    [skip ci] v1.4.0

--- punq-dev.rb
+++ punq-dev.rb
@@ -4,62 +4,62 @@ class PunqDev < Formula
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mogenius/homebrew-punq-dev/releases/download/v1.4.0-develop.3/punq-dev-v1.4.0-develop.3-darwin-arm64.tar.gz"
-      sha256 "a72b51544e1cf704ca431ffc13606238c6766006330bd9c0c4838742dba2fdf9"
+      url "https://github.com/mogenius/homebrew-punq-dev/releases/download/v1.4.0/punq-dev-v1.4.0-darwin-arm64.tar.gz"
+      sha256 ""
     elsif Hardware::CPU.intel?
-      url "https://github.com/mogenius/homebrew-punq-dev/releases/download/v1.4.0-develop.3/punq-dev-v1.4.0-develop.3-darwin-amd64.tar.gz"
-      sha256 "476d01c6333837d81c269300618becbb70d2a581a3ab787922ee0778ffc1612c"
+      url "https://github.com/mogenius/homebrew-punq-dev/releases/download/v1.4.0/punq-dev-v1.4.0-darwin-amd64.tar.gz"
+      sha256 ""
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mogenius/homebrew-punq-dev/releases/download/v1.4.0-develop.3/punq-dev-v1.4.0-develop.3-linux-amd64.tar.gz"
-        sha256 "4d4d9e7f64e88d48ee502c256bd9be764ef604b5fe3ebf9fc92a233a7ecaed53"
+        url "https://github.com/mogenius/homebrew-punq-dev/releases/download/v1.4.0/punq-dev-v1.4.0-linux-amd64.tar.gz"
+        sha256 ""
       else
-        url "https://github.com/mogenius/homebrew-punq-dev/releases/download/v1.4.0-develop.3/punq-dev-v1.4.0-develop.3-linux-386.tar.gz"
-        sha256 "7bc8c9229a15f0a88637abd4b67594924596b7f9c47df31b8388126aa696c601"
+        url "https://github.com/mogenius/homebrew-punq-dev/releases/download/v1.4.0/punq-dev-v1.4.0-linux-386.tar.gz"
+        sha256 ""
       end
     elif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mogenius/homebrew-punq-dev/releases/download/v1.4.0-develop.3/punq-dev-v1.4.0-develop.3-linux-arm64.tar.gz"
-        sha256 "173ae1b9ecf49ebaca956eecf824b05db8487b7de588f6a770c395c3fbed881e"
+        url "https://github.com/mogenius/homebrew-punq-dev/releases/download/v1.4.0/punq-dev-v1.4.0-linux-arm64.tar.gz"
+        sha256 ""
       else
-        url "https://github.com/mogenius/homebrew-punq-dev/releases/download/v1.4.0-develop.3/punq-dev-v1.4.0-develop.3-linux-arm.tar.gz"
-        sha256 "c6fea09ef000ddf6e67816854b212297758350274db1073304262dca75461a0e"
+        url "https://github.com/mogenius/homebrew-punq-dev/releases/download/v1.4.0/punq-dev-v1.4.0-linux-arm.tar.gz"
+        sha256 ""
       end
     end
   end
   
-  version "1.4.0-develop.3"
+  version "1.4.0"
   license "MIT"
 
   def install
   if OS.mac?
     if Hardware::CPU.arm?
       # Installation steps for macOS ARM64
-      bin.install "punq-dev-v1.4.0-develop.3-darwin-arm64" => "punq-dev"
+      bin.install "punq-dev-v1.4.0-darwin-arm64" => "punq-dev"
     elsif Hardware::CPU.intel?
       # Installation steps for macOS AMD64
-      bin.install "punq-dev-v1.4.0-develop.3-darwin-amd64" => "punq-dev"
+      bin.install "punq-dev-v1.4.0-darwin-amd64" => "punq-dev"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         # Installation steps for Linux AMD64
-        bin.install "punq-dev-v1.4.0-develop.3-linux-amd64" => "punq-dev"
+        bin.install "punq-dev-v1.4.0-linux-amd64" => "punq-dev"
       else
         # Installation steps for Linux 386
-        bin.install "punq-dev-v1.4.0-develop.3-linux-386" => "punq-dev"
+        bin.install "punq-dev-v1.4.0-linux-386" => "punq-dev"
       end
     elsif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         # Installation steps for Linux ARM64
-        bin.install "punq-dev-v1.4.0-develop.3-linux-arm64" => "punq-dev"
+        bin.install "punq-dev-v1.4.0-linux-arm64" => "punq-dev"
       else
         # Installation steps for Linux ARM
-        bin.install "punq-dev-v1.4.0-develop.3-linux-arm" => "punq-dev"
+        bin.install "punq-dev-v1.4.0-linux-arm" => "punq-dev"
       end
     end
   end
